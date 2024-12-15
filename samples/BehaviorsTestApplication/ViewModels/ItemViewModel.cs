2c0ae7bedbb4dea5a48effac781f030660d0829d
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2024-12-15 22:23:47 +0100

    Use Reactive attribute

--- samples/BehaviorsTestApplication/ViewModels/ItemViewModel.cs
+++ samples/BehaviorsTestApplication/ViewModels/ItemViewModel.cs
@@ -1,24 +1,19 @@
 ﻿using System.Collections.ObjectModel;
-using ReactiveUI;
 
 namespace BehaviorsTestApplication.ViewModels;
 
-public class ItemViewModel(string value) : ViewModelBase
+public partial class ItemViewModel : ViewModelBase
 {
-    private ObservableCollection<ItemViewModel>? _items;
-    private string? _value = value;
-
-    public string? Value
+    public ItemViewModel(string value)
     {
-        get => _value;
-        set => this.RaiseAndSetIfChanged(ref _value, value);
+        _value = value;
     }
 
-    public ObservableCollection<ItemViewModel>? Items
-    {
-        get => _items;
-        set => this.RaiseAndSetIfChanged(ref _items, value);
-    }
+    [Reactive]
+    public partial string? Value { get; set; }
+
+    [Reactive]
+    public partial ObservableCollection<ItemViewModel>? Items { get; set; }
 
     public override string ToString() => _value ?? string.Empty;
 }
