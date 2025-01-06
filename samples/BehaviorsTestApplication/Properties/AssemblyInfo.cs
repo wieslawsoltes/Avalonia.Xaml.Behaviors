5e45b5a596867a3b7c19f86aec718f8b8932a310
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-01-06 19:48:31 +0100

    Create AssemblyInfo.cs

---
+++ samples/BehaviorsTestApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+using Avalonia.Metadata;
+
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "BehaviorsTestApplication")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "BehaviorsTestApplication.Controls")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "BehaviorsTestApplication.Converters")]
