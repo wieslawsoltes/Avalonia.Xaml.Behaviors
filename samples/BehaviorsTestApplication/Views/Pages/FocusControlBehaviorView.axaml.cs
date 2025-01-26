84f03365d7fef5324fd1d0efee5bbf421147395e
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-01-26 19:14:31 +0100

    Add FocusControlBehavior page

---
+++ samples/BehaviorsTestApplication/Views/Pages/FocusControlBehaviorView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Views.Pages;
+
+public partial class FocusControlBehaviorView : UserControl
+{
+    public FocusControlBehaviorView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
