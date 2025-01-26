73264d6f6d6b22aa69baa1cd0c02743b8d1fa1f1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-01-26 18:14:40 +0100

    Add DataTriggerBehaviorAdvancedView

---
+++ samples/BehaviorsTestApplication/Views/Pages/DataTriggerBehaviorAdvancedView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Views.Pages;
+
+public partial class DataTriggerBehaviorAdvancedView : UserControl
+{
+    public DataTriggerBehaviorAdvancedView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
