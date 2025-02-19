cf2ac9ec0ac825e658c1edeee336cfc91062a688
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-02-19 20:54:08 +0100

    Add example

---
+++ samples/BehaviorsTestApplication/Views/Pages/OneTimeBinding.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace BehaviorsTestApplication.Views.Pages;
+
+public partial class OneTimeBinding : UserControl
+{
+    public OneTimeBinding()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
