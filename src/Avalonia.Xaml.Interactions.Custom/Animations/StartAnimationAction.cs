91e6e5fa6867d66f154f8b25ce9c0766f135b506
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-03-03 20:18:56 +0100

    Add StartAnimationAction

---
+++ src/Avalonia.Xaml.Interactions.Custom/Animations/StartAnimationAction.cs
@@ -0,0 +1,43 @@
+using Avalonia.Controls;
+using Avalonia.Xaml.Interactivity;
+
+namespace Avalonia.Xaml.Interactions;
+
+/// <summary>
+/// 
+/// </summary>
+public class StartAnimationAction : AvaloniaObject, IAction
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static readonly StyledProperty<Animation.Animation?> AnimationProperty =
+        AvaloniaProperty.Register<StartAnimationAction, Animation.Animation?>(nameof(Animation));
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public Animation.Animation? Animation
+    {
+        get => GetValue(AnimationProperty);
+        set => SetValue(AnimationProperty, value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="parameter"></param>
+    /// <returns></returns>
+    public object Execute(object? sender, object? parameter)
+    {
+        if (sender is not Control control || Animation is null)
+        {
+            return false;
+        }
+
+        _ = Animation.RunAsync(control);
+
+        return true;
+    }
+}
