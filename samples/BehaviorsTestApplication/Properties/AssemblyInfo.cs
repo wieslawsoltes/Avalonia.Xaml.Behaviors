f96ea5482ab9c93db11c2e350a54c396039fd2f0
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-03-04 12:37:26 +0100

    Add XmlnsDefinition for BehaviorsTestApplication.Behaviors

--- samples/BehaviorsTestApplication/Properties/AssemblyInfo.cs
+++ samples/BehaviorsTestApplication/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "BehaviorsTestApplication")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "BehaviorsTestApplication.Controls")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "BehaviorsTestApplication.Converters")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "BehaviorsTestApplication.Behaviors")]
